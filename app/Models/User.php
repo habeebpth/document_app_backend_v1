3a2e71dc6c8d1be4fba00d0fb37bc24655b61142
jafarali862 <jafar.u123@gmail.com>
2025-11-29 15:42:29 +0530

    Updated api

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-<<<<<<< HEAD
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -31,34 +30,4 @@ class User extends Authenticatable
         'created_at' => 'datetime:Y-m-d H:i:s',
         'updated_at' => 'datetime:Y-m-d H:i:s'
     ];
-=======
-
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
-
-
-    protected $fillable = ['name','email','password','mobile_number','whatsapp_number','user_name','avatar'];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = ['password','remember_token'];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = ['email_verified_at' => 'datetime','created_at' => 'datetime:Y-m-d H:i:s','updated_at' => 'datetime:Y-m-d H:i:s'];
-
->>>>>>> 9953726449f6cec3f1c51ad7166a16ea263cae0b
 }
